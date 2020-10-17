7f3c926641530f14a57e7406ae9244d083f96fc1
Soham Das <majumder70.ranu@gmail.com>
2020-10-17 18:32:10 +0530

    defined the primary layouts

--- weather.py
+++ weather.py
@@ -1,16 +1,26 @@
 import tkinter
-
+from tkinter import BOTH
 root = tkinter.Tk()
 root.title('Weathe App')
 root.iconbitmap('weather.ico')
-root.geometry('300x300')
+root.geometry('400x400')
 root.resizable(0,0)
 root.config(bg="#F9A620")
 
 #define colors and fonts
+sky_color = "#9FC2CC"
+grass_color = "#20BF55"
 input_color = "#548C2F"
 output_color = "#1B5299"
 large_font = ('SimSun', 14)
 small_font = ('SimSun', 10)
+
+#define functions
+
+#define layout
+sky_frame = tkinter.Frame(root, bg=sky_color, height=250)
+grass_frame = tkinter.Frame(root, bg=grass_color)
+sky_frame.pack(fill=BOTH, expand=True)
+grass_frame.pack(fill=BOTH, expand=True)
 
 root.mainloop()
