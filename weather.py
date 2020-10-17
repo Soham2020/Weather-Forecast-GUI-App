6c4d7e512bad4141e037e04eaee1253c803b825a
Soham Das <majumder70.ranu@gmail.com>
2020-10-17 20:09:30 +0530

    serach function added

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter, requests
 from tkinter import BOTH, IntVar
 root = tkinter.Tk()
 root.title('Weathe App')
@@ -9,13 +9,25 @@ root.resizable(0,0)
 #define colors and fonts
 sky_color = "#9FC2CC"
 grass_color = "#20BF55"
-input_color = "#548C2F"
+input_color = "#50FFB1"
 output_color = "#01BAEF"
 large_font = ('SimSun', 14)
 small_font = ('SimSun', 10)
 
 #define functions
+def search():
+    global response
 
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    api_key = "a21df2a0766cf894f487aedc536bc0e4"
+    if search_method.get() == 1:
+        querystring = {"q" : city_entry.get(), "appid":api_key}
+    elif search_method.get() == 2:
+        querystring = {"q" : city_entry.get(), "appid":api_key}
+
+    response = requests.request("GET", url, params=querystring)
+    response = response.json()
+        
 #define layout
 sky_frame = tkinter.Frame(root, bg=sky_color, height=250)
 grass_frame = tkinter.Frame(root, bg=grass_color)
@@ -46,7 +58,7 @@ photo_label.pack(pady=8)
 
 #input frame layout
 city_entry = tkinter.Entry(input_frame, width=20, font=large_font)
-submit_button = tkinter.Button(input_frame, text="Submit", bg=input_color, font=large_font)
+submit_button = tkinter.Button(input_frame, text="Submit", bg=input_color, font=large_font, command=search)
 search_method = IntVar()
 search_method.set(1)
 search_city = tkinter.Radiobutton(input_frame, text="Submit Search City", variable=search_method, value=1, bg=input_color, font=small_font)
