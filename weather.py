99356056d60466e4d1cdc193d200ac07bf4c355a
Soham Das <majumder70.ranu@gmail.com>
2020-10-17 21:39:44 +0530

    added function get_weather and icon_image

--- weather.py
+++ weather.py
@@ -1,5 +1,7 @@
 import tkinter, requests
 from tkinter import BOTH, IntVar
+from PIL import ImageTk, Image
+from io import BytesIO
 root = tkinter.Tk()
 root.title('Weathe App')
 root.iconbitmap('weather.ico')
@@ -10,7 +12,7 @@ root.resizable(0,0)
 sky_color = "#9FC2CC"
 grass_color = "#20BF55"
 input_color = "#50FFB1"
-output_color = "#01BAEF"
+output_color = "#81F7E5"
 large_font = ('SimSun', 14)
 small_font = ('SimSun', 10)
 
@@ -21,13 +23,53 @@ def search():
     url = "https://api.openweathermap.org/data/2.5/weather"
     api_key = "a21df2a0766cf894f487aedc536bc0e4"
     if search_method.get() == 1:
-        querystring = {"q" : city_entry.get(), "appid":api_key}
+        querystring = {"q" : city_entry.get(), "appid":api_key, "units": "metric"}
     elif search_method.get() == 2:
-        querystring = {"q" : city_entry.get(), "appid":api_key}
+        querystring = {"q" : city_entry.get(), "appid":api_key, "units":"metric"}
 
     response = requests.request("GET", url, params=querystring)
     response = response.json()
-        
+
+    get_weather()
+    icon_image()
+
+def get_weather():
+    city_name = response['name']
+    city_lat = str(response['coord']['lat'])
+    city_lon = str(response['coord']['lon'])        
+    main_weather = response['weather'][0]['main']
+    description = response['weather'][0]['description']
+    temp = str(response['main']['temp'])
+    feels_like = str(response['main']['feels_like'])
+    temp_min = str(response['main']['temp_min'])
+    temp_max = str(response['main']['temp_max'])
+    pressure = str(response['main']['pressure'])
+    humidity = str(response['main']['humidity'])
+
+    #update output labels
+    city_info_label.config(text=city_name + "(" + city_lat + "," + city_lon + ")", font=large_font, bg=output_color)
+    weather_label.config(text="Weather is::  " + main_weather + " , " + description, font=small_font, bg=output_color)
+    temp_label.config(text="Temparatur is :: " + temp + " C", font=small_font, bg=output_color)
+    temp_max_label.config(text="Maximum Temparature is :: " + temp_max + " C", font=small_font, bg=output_color)
+    temp_min_label.config(text="Minimum Temparature is :: " + temp_min + " C", font=small_font, bg=output_color)
+    humidity_label.config(text="Humidity is :: " + humidity, font=small_font, bg=output_color)
+    feels_label.config(text="Feels like :: " + feels_like + " C", font=small_font, bg=output_color)
+    pressure_label.config(text="Pressure is :: " + pressure + " Pa", font=small_font, bg=output_color)
+
+def icon_image():
+    global img
+    icon_id = response['weather'][0]['icon']
+    # print(icon_id)
+    url = 'http://openweathermap.org/img/wn/{icon}.png'.format(icon=icon_id)
+    icon_response = requests.get(url, stream=True)
+    # print(icon_response)
+
+    img_data = icon_response.content
+    # print(img_data)
+    img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+    # print(img)
+    photo_label.config(image=img)
+
 #define layout
 sky_frame = tkinter.Frame(root, bg=sky_color, height=250)
 grass_frame = tkinter.Frame(root, bg=grass_color)
@@ -39,14 +81,15 @@ output_frame.pack(pady=30)
 output_frame.pack_propagate(0)
 input_frame.pack(pady=15)
 #output frame layout
-city_info_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-weather_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-temp_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-feels_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-temp_min_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-temp_max_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-humidity_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
-photo_label = tkinter.Label(output_frame, bg=output_color, text='Testing')
+city_info_label = tkinter.Label(output_frame, bg=output_color)
+weather_label = tkinter.Label(output_frame, bg=output_color)
+temp_label = tkinter.Label(output_frame, bg=output_color)
+feels_label = tkinter.Label(output_frame, bg=output_color)
+temp_min_label = tkinter.Label(output_frame, bg=output_color)
+temp_max_label = tkinter.Label(output_frame, bg=output_color)
+humidity_label = tkinter.Label(output_frame, bg=output_color)
+pressure_label = tkinter.Label(output_frame, bg=output_color)
+photo_label = tkinter.Label(output_frame, bg=output_color)
 city_info_label.pack(pady=8)
 weather_label.pack()
 temp_label.pack()
@@ -54,6 +97,7 @@ feels_label.pack()
 temp_min_label.pack()
 temp_max_label.pack()
 humidity_label.pack()
+pressure_label.pack()
 photo_label.pack(pady=8)
 
 #input frame layout
